3ffb04bf61962b5cb1d7390f133a64845eb46c48
Helge Milde <helgefmi@gmail.com>
2014-04-06 12:25:41 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ pwd
 rm -rf build/
 
 mkdir build/Unicorn/Unicorn -p
-cp -ar $(egrep -v '^#|^$' Unicorn.txt.release) build/Unicorn/
+cp -ar *.lua build/Unicorn/
+cp -ar Unicorn/*.lua build/Unicorn/Unicorn/
 cp -ar Unicorn.txt.release build/Unicorn/Unicorn.txt
 cp -ar *.md MIT-LICENSE build/Unicorn/
